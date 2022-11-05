7e39e7adcfa06ca3a31e9cf9f659ef9ce4792db2
Александра Иванова <okshurochka@yandex.ru>
2022-11-05 23:43:45 +0300

    Create new solution

---
+++ MyLessonApp/MyLessonApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLessonApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
